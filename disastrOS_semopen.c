3e33c0e3f6b1c9732446fb2f88fe36f2caafd6da
Andrea Franceschi <franceschi.1709888@studenti.uniroma1.it>
2018-04-12 11:01:31 +0200

    Successiva implementazine della sem_open con dichiarazioni delle costanti per la gestione degli errori in costants.h

--- disastrOS_semopen.c
+++ disastrOS_semopen.c
@@ -13,8 +13,10 @@ void internal_semOpen(){
 
     int sem_val = running->syscall_args[1];
 
+    int ultimo_sem_usato = running->last_sem_fd;
+
     //creo il semaforo allocandolo con sem_num = n_semaforo
-    Semaphore* sem = Semaphore_alloc(semnum, value);
+    Semaphore* sem = Semaphore_alloc(n_semaforo, sem_val);
     printf( "Alloco il semaforo con sem_num = %d\n", n_semaforo);
     if(!sem){
         running->syscall_retvalue = DSOS_ECREATESEM;
@@ -26,8 +28,26 @@ void internal_semOpen(){
     SemDescriptor* sem_aperto = check_id(&semafori_aperti, n_semaforo);
     if( sem_aperto ){
         running->syscall_retvalue = sem_aperto->fd;
+        return;
     }
 
+    //incremento il last_sem_fd in modo tale che il prossimo semaforo che il processo aprirà avrà l'fd diverso dal precedente
+    (running->last_sem_fd)+=1;
 
+    //creo un descrittore che aggiungero al PCB del processo chiamante
+    SemDescriptor* desc_pcb = SemDescriptor_alloc(ultimo_sem_usato, sem, running);
+    if(!desc_pcb){
+        running->syscall_retvalue = DSOS_ECREATESEM;
+        return;
+    }
+
+    //alloco il puntatore al semaforo
+    SemDescriptor* puntatore_sem = SemDescriptorPtr_alloc(desc_pcb);
+    if(!puntatore_sem){
+        running->syscall_retvalue = DSOS_ECREATEPTR;
+        return;
+    }
+
+    desc_pcb->ptr = puntatore_sem;
 
 }
