396ce12661ed2612dfda4d6b845f33e048ea9d77
federico bucci <bucci.1649197@studenti.uniroma1.it>
2018-05-12 11:45:41 +0200

    inizio scrittura codice semClose

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,5 +7,44 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-  // do stuff :)
+  int fd= running->syscall_args[0];
+
+        SemDescriptor* semD= SemDescriptorList_byFd(&running->sem_descriptors, fd);
+        if(!semD)
+        {
+        running->syscall_retvalue=-1;
+        }
+
+        //prendo il semaforo del semD
+        Semaphore* sem= semD->semaphore;
+
+        //prendo i descrittori associati e li andro ad eliminare
+        SemDescriptorPtr* semD_ptr = semD->ptr;
+
+
+        semD_ptr=(SemDescriptorPtr*) List_detach(&(sem->descriptors),(ListItem*) semD_ptr);
+        int r=SemDescriptor_free(semD_ptr);
+        if(r)
+        {
+            running->syscall_retvalue=r;
+            return;
+        }
+
+        SemDescriptorPtr* semD_wait = semD->ptr_wait;
+        r=SemDescriptorPtr_free(semD_wait)
+        if(r){
+            running->syscall_retvalue=r;
+            return;
+        }
+
+        //free del puntatore a semD della lista dei descrittori del processo
+        semD=(SemDescriptor*) List_detach(&(running->sem_descriptors), (ListItem*) semD);
+        r=SemDescriptor_free(semD);
+        if(r){
+            running->syscall_retvalue=r;
+            return;
+        }
+
+        running->syscall_retvalue = 0;
+
 }
